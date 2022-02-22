cd1abeaca7b26f9c3a4b3da6af41ce0e672a0cf3
NetraR <netravatigond1997@gmail.com>
2022-02-22 21:21:43 +0530

    addr1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { Component } from 'react';
+import ClassComponent from './components/ClassComponent';
+import './App.css';
+import FuncComponent from './components/FuncComponent';
+
+class App extends Component {
+  state = {
+    isClass: false,
+    isFunctional: false,
+  };
+  render() {
+    return (
+      <div className='App'>
+        <div className='Header'>
+          <h1>Styling using Functional and Class Components</h1>
+        </div>
+        <div className='button'>
+          <button
+            onClick={() =>
+              this.setState({ isFunctional: !this.state.isFunctional })
+            }
+          >
+            To see styling in Functional Component
+          </button>
+          <button
+            onClick={() => this.setState({ isClass: !this.state.isClass })}
+          >
+            To see styling in Class Component
+          </button>
+        </div>
+        <div className='container'>
+          {this.state.isFunctional ? <FuncComponent /> : null}
+          {this.state.isClass ? <ClassComponent /> : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
